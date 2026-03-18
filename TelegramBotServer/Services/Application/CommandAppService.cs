d9edfcc240842894d31e52d2a2ab2492196b40e7
erkebulan777@gmail.com <erkebulan777@gmail.com>
2026-03-18 11:15:45 +0500

    Обновлено приветственное сообщение бота
    Улучшен и сокращён текст приветствия: добавлен перевод строки, обновлены и уточнены описания команд (/export, /automation, /status), убран формат IFC из списка экспорта. Текст стал более лаконичным и структурированным.

--- TelegramBotServer/Services/Application/CommandAppService.cs
+++ TelegramBotServer/Services/Application/CommandAppService.cs
@@ -187,12 +187,11 @@ public sealed class CommandAppService : ICommandAppService
     {
         var startText = new StringBuilder()
             .AppendLine($"Привет, {username}! 👋")
-            .AppendLine("Я бот для работы с BIM-документами, автоматизации задач и экспорта файлов.")
-            .AppendLine()
+            .AppendLine("Я бот для работы с BIM-документами, автоматизации задач и экспорта файлов.\n")
             .AppendLine("Вот что я умею (нажмите на команду):")
-            .AppendLine("🔹 /export - экспорт в форматы PDF, DWG, NWC, IFC")
-            .AppendLine("🔹 /automation - задачи автоматизации (BIM Doctor, Clash Report и др.)")
-            .AppendLine("🔹 /status - проверка состояния выполнения ваших задач")
+            .AppendLine("🔹 /export - экспорт в форматы PDF, DWG, NWC")
+            .AppendLine("🔹 /automation - задачи BIM автоматизации ")
+            .AppendLine("🔹 /status - состояния выполнения задач")
             .AppendLine("🔹 /help - показать подробную справку")
             .ToString();
 
