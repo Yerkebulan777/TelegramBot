4003b20d4e877570c15570a076db251d81c8683f
Yerkebulan777 <erkebulan777@gmail.com>
2026-07-08 15:07:58 +0500

    Reorder DATA command after IFC

--- TelegramBot.Server/Models/CommandDefinition.cs
+++ TelegramBot.Server/Models/CommandDefinition.cs
@@ -21,8 +21,8 @@ public static class CommandCatalog
         new(CommandCodes.Pdf, "Export to PDF", CallbackPrefixes.Pdf, CommandGroup.Export),
         new(CommandCodes.Dwg, "Export to DWG", CallbackPrefixes.Dwg, CommandGroup.Export),
         new(CommandCodes.Nwc, "Export to NWC", CallbackPrefixes.Nwc, CommandGroup.Export),
-        new(CommandCodes.Data, "Export DATA", CallbackPrefixes.Data, CommandGroup.Export),
         new(CommandCodes.Ifc, "Export to IFC", CallbackPrefixes.Ifc, CommandGroup.Export),
+        new(CommandCodes.Data, "Export DATA", CallbackPrefixes.Data, CommandGroup.Export),
         new(CommandCodes.BimDoc, "BIM Doctor", CallbackPrefixes.BimDoc, CommandGroup.Automation),
         new(CommandCodes.ClashRep, "Clash Report", CallbackPrefixes.ClashRep, CommandGroup.Automation),
         new(CommandCodes.AutoRes, "Auto Resolver", CallbackPrefixes.AutoRes, CommandGroup.Automation)
